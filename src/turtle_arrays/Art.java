67ed6c39971841d46b3e233d6e3d9c92fada6fd8
nguyenle <nguyenle@nguyens-macbook-pro.home>
2020-11-06 12:40:25 +0100

    .

--- src/turtle_arrays/Art.java
+++ src/turtle_arrays/Art.java
@@ -2,9 +2,14 @@ package turtle_arrays;
 
 import tbs.gfx.Turtle;
 import tbs.simpleapp.SimpleApp;
+import java.util.Random;
 
 public class Art extends SimpleApp{
+
 	Turtle t = screen.createTurtle();
+	Random random = new Random();
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		new Art();
@@ -13,7 +18,8 @@ public class Art extends SimpleApp{
 	public void main() 
 	{
 		for(int i = 0; i<300; i+=1) {
-			t.setColour((int)(Math.random() * 0x1000000));
+			int color = random.nextInt(0xffffff);
+			t.setColour(color);
 			t.move(i);
 			t.rotate(98);
 		}
